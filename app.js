e93d0b69be028e90c81350c7beb20c4899436571
Flatcova <m.covar20@gmail.com>
2017-03-28 22:01:33 -0600

    se arreglo el layout para poder usar la nueva vista en todas las secciones, se agrego el url /signup para realizar el registro de un doctor. se creo una nueva carpeta para los procesos de registro y se limpio el documento de javascript

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var signup = require('./routes/account/signup');
 
 var app = express();
 var engine = require('ejs-mate');
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/signup', signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
